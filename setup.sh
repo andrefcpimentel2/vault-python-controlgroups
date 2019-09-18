1c65530de2e31f002e6ffac5c06234456c6b7d60
Ricardo Oliveira <ricardo@hashicorp.com>
2019-09-18 11:28:13 +0100

    adding the requesters group

--- setup.sh
+++ setup.sh
@@ -10,16 +10,27 @@ vault write auth/ldap/config \
     groupdn="ou=Groups,dc=example,dc=org" \
     insecure_tls=true
 
+vault auth list -format=json  | jq -r '.["ldap/"].accessor' > accessor.txt
+
 vault write identity/group name="approvers" \
       policies="superuser" \
       type="external"
 
 vault read identity/group/name/approvers  -format=json | jq -r .data.id > approvers_group_id.txt
-vault auth list -format=json  | jq -r '.["ldap/"].accessor' > accessor.txt
 
 vault write identity/group-alias name="approvers" \
         mount_accessor=$(cat accessor.txt) \
         canonical_id=$(cat approvers_group_id.txt)
 
+
+vault write identity/group name="requesters" \
+      policies="test" \
+      type="external"
+
+vault read identity/group/name/requesters  -format=json | jq -r .data.id > requesters_group_id.txt
+
+vault write identity/group-alias name="requesters" \
+        mount_accessor=$(cat accessor.txt) \
+        canonical_id=$(cat requesters_group_id.txt)
 
 vault kv put kv/cgtest example=value
